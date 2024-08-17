3ef63ad6ff0859485e4e59d24795aa54be60240f
Mardoqueu Sousa Telvina <mardoqueu.telvina@basisti.com.br>
2024-08-17 02:26:55 -0300

    Add RecordRepository for accessing record data

---
+++ src/main/java/com/example/calculator/repository/OperationRepository.java
@@ -0,0 +1,7 @@
+package com.example.calculator.repository;
+
+import com.example.calculator.entity.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OperationRepository extends JpaRepository<Operation, Long> {
+}
