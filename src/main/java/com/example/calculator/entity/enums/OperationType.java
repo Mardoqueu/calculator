edf70bf5609ac1626bf295f8dab7aa51500a419a
Mardoqueu Sousa Telvina <mardoqueu.telvina@basisti.com.br>
2024-08-17 02:24:57 -0300

    Add OperationType enum for supported operations

---
+++ src/main/java/com/example/calculator/entity/enums/OperationType.java
@@ -0,0 +1,10 @@
+package com.example.calculator.entity.enums;
+
+public enum OperationType {
+    ADDITION,
+    SUBTRACTION,
+    MULTIPLICATION,
+    DIVISION,
+    SQUARE_ROOT,
+    RANDOM_STRING
+}
